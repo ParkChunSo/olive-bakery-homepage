757c21ef0137fd321bab826dadf2c3cb423aba1a
ParkChunSo <cnsth123@gmail.com>
2019-11-24 21:21:25 +0900

    로그 설정 완료

--- src/main/java/com/dev/olivebakery/utill/MethodAop.java
+++ src/main/java/com/dev/olivebakery/utill/MethodAop.java
@@ -1,8 +1,12 @@
 package com.dev.olivebakery.utill;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.JoinPoint.StaticPart;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.SourceLocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -17,6 +21,22 @@ public class MethodAop {
   @Before("execution(* com.dev.olivebakery.service.*.*.*(..)) || execution(* com.dev.olivebakery.service.*.*(..))")
   public void startLog(JoinPoint jp) {
     logger.info("메소드 이름:" + jp.getSignature().getName());
+  }
+
+  @Before("execution(* com.dev.olivebakery.service.SignService.*(..))")
+  public void signServiceLog(JoinPoint jp){
+    Signature signature = jp.getSignature();
+    Object[] args = jp.getArgs();
+    String kind = jp.getKind();
+    SourceLocation sourceLocation = jp.getSourceLocation();
+    StaticPart staticPart = jp.getStaticPart();
+    Object target = jp.getTarget();
+    Object aThis = jp.getThis();
+  }
+
+  @Before("execution(* com.dev.olivebakery.security.*.*.*(..))")
+  public void tokenLog(JoinPoint jp){
+
   }
 
 }
